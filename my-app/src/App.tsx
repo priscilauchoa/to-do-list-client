419208cdba5eec92776160d85eb7f5524e94c54c
Priscila-Flores <priscilauchoaa@gmail.com>
2023-03-02 17:26:55 +0100

    import fecth hook

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./global.css"
+import { useFetchTask, Response } from './components/Tasks/hooks/useFetchTask';
 
 function App() {
+
+  const data: Response = useFetchTask(
+    'https://github.com'
+  );
+
+  if (!data.loading) console.log(data);
+
   return (
     <div className="App">
-      
+
     </div>
   );
 }
